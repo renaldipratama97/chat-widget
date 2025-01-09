fdb40f466e0d6d7ee9c916f52653164f761da26a
renaldipratama51@gmail.com <renaldipratama51@gmail.com>
2025-01-09 13:13:20 +0700

    utility

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import Glitch from './Glitch.vue'
+import Utility from './Utility.vue'
 </script>
 
 <template>
@@ -8,7 +9,7 @@ import Glitch from './Glitch.vue'
       <div class="w-10 h-10 rounded-[50%] bg-red-500 flex justify-center items-center text-white text-xl font-bold">
         A
       </div>
-      <div class="flex flex-col justify-center w-[90%]">
+      <div class="flex gap-2 flex-col justify-center w-[90%]">
         <div class="bg-gray-100 w-max max-w-[80%] rounded-b-md rounded-tr-md px-2 pb-2 pt-1">
           <div class="text-red-500 font-bold italic">Admin TRR</div>
           <div>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Provident asperiores aliquid fugit quod quidem
@@ -20,21 +21,25 @@ import Glitch from './Glitch.vue'
             itaque exercitationem ipsum quia voluptates vel est sit aspernatur nihil laudantium unde laboriosam alias
             cum.</div>
         </div>
+        <Utility />
       </div>
     </div>
     <br>
+
     <div id="user" class="flex flex-row-reverse gap-1">
       <div class="w-10 h-10 rounded-[50%] bg-red-500 flex justify-center items-center text-white text-xl font-bold">
         U
       </div>
-      <div class="flex flex-col items-end w-[90%]">
+      <div class="flex gap-2 flex-col items-end w-[90%]">
         <div class="bg-gray-100 w-max max-w-[80%] rounded-b-md rounded-tl-md px-2 pb-2 pt-1">
           <div class="text-red-500 font-bold italic">User TRR</div>
           <div>Hi</div>
         </div>
+        <Utility />
       </div>
     </div>
     <br>
+
     <div id="admin" class="flex gap-1">
       <div class="w-10 h-10 rounded-[50%] bg-red-500 flex justify-center items-center text-white text-xl font-bold">
         A
---
+++ src/components/Utility.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+</script>
+
+<template>
+
+  <div class="icons flex gap-2">
+    <div id="copy" class="w-7 h-7 p-1 hover:bg-gray-200 rounded-md cursor-pointer">
+      <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 1024 1024">
+        <path
+          d="M832 64H296c-4.4 0-8 3.6-8 8v56c0 4.4 3.6 8 8 8h496v688c0 4.4 3.6 8 8 8h56c4.4 0 8-3.6 8-8V96c0-17.7-14.3-32-32-32zM704 192H192c-17.7 0-32 14.3-32 32v530.7c0 8.5 3.4 16.6 9.4 22.6l173.3 173.3c2.2 2.2 4.7 4 7.4 5.5v1.9h4.2c3.5 1.3 7.2 2 11 2H704c17.7 0 32-14.3 32-32V224c0-17.7-14.3-32-32-32zM350 856.2L263.9 770H350v86.2zM664 888H414V746c0-22.1-17.9-40-40-40H232V264h432v624z"
+          fill="currentColor"></path>
+      </svg>
+    </div>
+    <div id="download" class="w-7 h-7 p-1 hover:bg-gray-200 rounded-md cursor-pointer">
+      <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 1024 1024">
+        <path
+          d="M505.7 661a8 8 0 0 0 12.6 0l112-141.7c4.1-5.2.4-12.9-6.3-12.9h-74.1V168c0-4.4-3.6-8-8-8h-60c-4.4 0-8 3.6-8 8v338.3H400c-6.7 0-10.4 7.7-6.3 12.9l112 141.8zM878 626h-60c-4.4 0-8 3.6-8 8v154H214V634c0-4.4-3.6-8-8-8h-60c-4.4 0-8 3.6-8 8v198c0 17.7 14.3 32 32 32h684c17.7 0 32-14.3 32-32V634c0-4.4-3.6-8-8-8z"
+          fill="currentColor"></path>
+      </svg>
+    </div>
+  </div>
+
+</template>
